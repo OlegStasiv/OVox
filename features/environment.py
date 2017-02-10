e019b4ea9b7ef0d39973e8fa6764913c537309ef
thinkmobilesqa <oleg.stasiv@thinkmobiles.com>
2017-02-10 17:00:27 +0200

    remove xvbc

--- features/environment.py
+++ features/environment.py
@@ -6,11 +6,10 @@ from selenium.webdriver.chrome.options import Options
 
 
 def before_scenario(context, scenario):
-    from xvfbwrapper import Xvfb
     # xf = Xvfb(1920, 1080)  # xf = Xvfb(1920, 1080) - will create virtual display with 1920x1080 size
     # xf.start()
-    display = Display(visible=1, size=(1920, 1080))
-    display.start()
+    # display = Display(visible=1, size=(1920, 1080))
+    # display.start()
     context.browser = webdriver.Chrome(executable_path='{}/chromedriver'.format(os.getcwd()))
     #context.browser = webdriver.Chrome(executable_path='{}/chromedriver'.format(os.getcwd()))
     # context.browser = webdriver.Chrome() if you have set chromedriver in your PATH
