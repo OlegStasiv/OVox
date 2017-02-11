70749e1a2d5fc911eb69448862b3618f20bdeea6
thinkmobilesqa <oleg.stasiv@thinkmobiles.com>
2017-02-11 17:35:44 +0200

    connect display

--- features/environment.py
+++ features/environment.py
@@ -8,8 +8,8 @@ from selenium.webdriver.chrome.options import Options
 def before_scenario(context, scenario):
     # xf = Xvfb(1920, 1080)  # xf = Xvfb(1920, 1080) - will create virtual display with 1920x1080 size
     # xf.start()
-    # display = Display(visible=1, size=(1920, 1080))
-    # display.start()
+    display = Display(visible=1, size=(1920, 1080))
+    display.start()
     context.browser = webdriver.Chrome(executable_path='{}/chromedriver'.format(os.getcwd()))
     #context.browser = webdriver.Chrome(executable_path='{}/chromedriver'.format(os.getcwd()))
     # context.browser = webdriver.Chrome() if you have set chromedriver in your PATH
