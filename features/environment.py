94c404fe4bd5b75a7b4cf73f13de0eb79f8215b8
thinkmobilesqa <oleg.stasiv@thinkmobiles.com>
2017-02-12 14:53:24 +0200

    1280x768

--- features/environment.py
+++ features/environment.py
@@ -8,7 +8,7 @@ from selenium.webdriver.chrome.options import Options
 def before_scenario(context, scenario):
     # xf = Xvfb(1920, 1080)  # xf = Xvfb(1920, 1080) - will create virtual display with 1920x1080 size
     # xf.start()
-    display = Display(visible=1, size=(1366, 768))
+    display = Display(visible=1, size=(1280, 768))
     display.start()
     context.browser = webdriver.Chrome(executable_path='{}/chromedriver'.format(os.getcwd()))
     #context.browser = webdriver.Chrome(executable_path='{}/chromedriver'.format(os.getcwd()))
@@ -16,6 +16,7 @@ def before_scenario(context, scenario):
 
     context.browser.set_page_load_timeout(300)
     context.browser.implicitly_wait(20)
+    context.browser.set_window_size(1280, 768)
     context.browser.maximize_window()
 
 
